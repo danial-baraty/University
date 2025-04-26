2b8a6b99d9055b0cbd12c88352cab53c474e948d
danial-baraty <150620024+danial-baraty@users.noreply.github.com>
2025-04-26 12:53:11 +0330

    first commit

---
+++ NetSec/HW1/NotSsl.py
@@ -0,0 +1,23 @@
+# BaseHTTPRequestHandler: A base class for handling HTTP requests in a server.
+# HTTPServer: A simple HTTP server class that listens for requests and uses a handler to process them.
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+
+        self.send_response(200)
+
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+
+        self.wfile.write(b"UNENCRYPTED TEXT!")
+
+
+# This ensures that the code following the if __name__ == "__main__": block is only executed when the file is run directly, not when it is imported as a module in another file.
+if __name__ == "__main__":
+    server_address = ("127.0.0.1", 8000) 
+    # When a GET request is sent to the server, HTTPServer creates an object from the handler class (in this case, SimpleHandler) and calls its do_GET method to process the request.
+    httpd = HTTPServer(server_address, SimpleHandler)
+    print("Server running at http://127.0.0.1:8000/")
+    httpd.serve_forever()
---
+++ NetSec/HW1/TLS.py
@@ -0,0 +1,56 @@
+
+# The program is written with two functions to improve structure and maintainability.
+# `create_ssl_server()` sets up the server, while `handle_client_connection()` manages client interactions.
+# This separation allows for easier updates and debugging, as changes to one part don't affect the other.
+
+import ssl
+import socket
+
+def create_ssl_server():
+    # SSL Layer
+    # SSLContext holds configuration for SSL/TLS connections (certificates, keys, protocols, etc.)
+    # It's used to wrap a socket and make it secure.
+
+    # Create an SSL context with the purpose of client authentication (though it's not enforced here)
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+
+    # Load the server's certificate and private key into the SSL context
+    # key.pem is the server's private key, cert.pem contains the public key (certificate)
+    context.load_cert_chain(certfile="c:/Users/Pc Dayan/Desktop/NetSec/HW1/cert.pem", keyfile="c:/Users/Pc Dayan/Desktop/NetSec/HW1/key.pem")
+
+    #TCP Layer
+    # Create a TCP/IP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Wrap the socket with SSL to create an SSL server socket
+    ssl_server_socket = context.wrap_socket(server_socket, server_side=True)
+    ssl_server_socket.bind(('127.0.0.1', 8443))
+    ssl_server_socket.listen(5) # When the server is busy and can't immediately accept all requests, it can hold up to 5 clients in the backlog queue
+    print("Server listening on port 8443...")
+    return ssl_server_socket
+
+
+def handle_client_connection(ssl_server_socket):
+    client_socket, addr = ssl_server_socket.accept()
+    print(f"Connection from {addr}")
+    data = client_socket.recv(1024)
+    # print(f"Received data: {data.decode('utf-8')}")
+ 
+    # Each HTTP header line ends with \r\n; headers end with an empty line: \r\n\r\n
+    body = "***HELLO from SSL Server***"
+    response = (
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/plain\r\n"
+    f"Content-Length: {len(body)}\r\n"
+    "Connection: close\r\n"
+    "\r\n"
+    f"{body}"
+                )
+    client_socket.sendall(response.encode('utf-8'))
+    client_socket.close()
+
+if __name__ == "__main__":
+    # SSL is responsible for securing the connection before passing data to the handler.
+    server_socket = create_ssl_server()
+    while True:
+        handle_client_connection(server_socket)
